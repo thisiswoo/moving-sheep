3643ee73b80d869e14108086aa6ebc8523d4180a
thisiswoo <thisiswoo0594@gmail.com>
2022-08-19 15:25:16 +0900

    edit app.js, hill.js

--- app.js
+++ app.js
@@ -1,48 +1,62 @@
 import {Hill} from './hill.js';
+// import {SheepController} from './sheep-controller.js';
+// import {Sun} from './Sun.js';
 
 class App {
-  constructor() {
-    this.canvas = document.createElement('canvas');
-    this.ctx = this.canvas.getContext('2d');
-    document.body.appendChild(this.canvas);
-    
-    this.hills = [
-      new Hill('#fd6bea', 0.2, 12),
-      new Hill('#ff59c2', 0.5, 8),
-      new Hill('#ff4674', 1.4, 6)
-    ];
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        document.body.appendChild(this.canvas);
 
-    window.addEventListener('resize', this.resize.bind(this), false);
-    this.resize();
+        // this.sun = new Sun();
 
-    requestAnimationFrame(this.animate.bind(this));
-  }
+        this.hills = [
+            new Hill('#fd6bea', 0.2, 12),
+            new Hill('#ff59c2', 0.5, 8),
+            new Hill('#ff4674', 1.4, 6)
+        ];
 
-  resize() {
-    this.stageWidth = document.body.clientWidth;
-    this.stageHeight = document.body.clientHeight;
+        // this.sheepController = new SheepController();
 
-    this.canvas.width = this.stageWidth * 2;
-    this.canvas.height = this.stageHeight * 2;
-    this.ctx.scale(2, 2);
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
 
-    for (let i = 0; i < this.hills.length; i++) {
-      this.hills[i].resize(this.stageWidth, this.stageHeight);
+        requestAnimationFrame(this.animate.bind(this));
     }
-  }
 
-  animate(t) {
-    requestAnimationFrame(this.animate.bind(this));
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
 
-    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+        this.canvas.width = this.stageWidth * 2;
+        this.canvas.height = this.stageHeight * 2;
+        this.ctx.scale(2, 2);
 
-    let dots;
-    for (let i = 0; i < this.hills.length; i++) {
-      dots = this.hills[i].draw(this.ctx);
+        // this.sun.resize(this.stageWidth, this.stageHeight);
+
+        for (let i = 0; i < this.hills.length; i++) {
+            this.hills[i].resize(this.stageWidth, this.stageHeight);
+        }
+
+        // this.sheepController.resize(this.stageWidth, this.stageHeight);
     }
-  }
+
+    animate(t) {
+        requestAnimationFrame(this.animate.bind(this));
+
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        // this.sun.draw(this.ctx, t);
+
+        let dots;
+        for (let i = 0; i < this.hills.length; i++) {
+            dots = this.hills[i].draw(this.ctx);
+        }
+
+        // this.sheepController.draw(this.ctx, t, dots);
+    }
 }
 
 window.onload = () => {
-  new App();
+    new App();
 };
